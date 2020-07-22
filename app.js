142f58c3f8461c113eba49957b636c1229d3f251
Lukas Dornstädter <lukas.dornstaedter@gmail.com>
2020-07-22 16:12:52 +0200

    get questions test

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Hello");
+const request = require("request");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+
+// Produkt Link
+const qaLink = 'https://www.amazon.de/ask/questions/asin/B0756CYWWD/';
+const qaPage = 1;
+const qaParameter = '/ref=ask_dp_iaw_ql_hza?isAnswered=true#question-Tx2B0DAZRJ1HTV8';
+
+
+// Get Amz Product HTML Code for testing
+/*
+request(`${qaLink}${qaPage}${qaParameter}`, (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        fs.writeFile("html.txt", JSON.stringify(html), ()=> {
+            console.log("done");
+        })
+
+    } else {
+        console.log(`Err on page ${page}`);
+
+    }
+})
+*/
+
+// work with the html test code 
+
+fs.readFile("html.txt", (err, data) => {
+    let $ = cheerio.load(JSON.parse(data));
+        $('.a-spacing-base ').each((i, el) => {
+        const review = {
+            productQuestion: $(el).find('.a-fixed-left-grid-inner').find('.a-col-right').find(".a-spacing-small").find("a").find(".a-declarative").text().trim()
+                }
+
+        if(review.productQuestion!=null && review.productQuestion != ""){
+        console.log(review);
+    }
+
+        
+    })
+})
+
+
+//            productQuestion: $(el).find('.a-fixed-left-grid-col .a-col-right').children().children().html()
